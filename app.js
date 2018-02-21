b3b4f491e5ef7305644f8bf7c719097723c70632
claudiaaw <claudiaaw95@gmail.com>
2018-02-21 21:24:05 +0800

    changed mongodb to env variable

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 //Import the mongoose module
 var mongoose = require('mongoose');
 //Set up default mongoose connection
-var mongoDB = 'mongodb://127.0.0.1:27017/locallibrary';
+var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB);
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
